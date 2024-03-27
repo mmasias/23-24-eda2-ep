f472d9ee3cd927fe92a1cfe5439520490e757f17
luciaZama <lzamanillol@gmail.com>
2024-03-27 21:58:57 +0100

    ##Clase App
    Se encarga de poner en marcha la aplicación

---
+++ Models/App.java
@@ -0,0 +1,9 @@
+package Models;
+
+public class App {
+    public static void main(String[] args) {
+        LibraryManager libraryManager = new LibraryManager();
+        libraryManager.manageLibrary();
+    }
+
+}
