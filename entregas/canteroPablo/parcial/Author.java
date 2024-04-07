327403ed8867d29bc72e470bdc31b362f90417b5
pabloocanteroo <pablo.cantero@alumnos.uneatlantico.es>
2024-04-07 19:06:42 +0200

    Cambios en los atributos, getters y constructor de la clase author

--- entregas/canteroPablo/parcial/Author.java
+++ entregas/canteroPablo/parcial/Author.java
@@ -1,20 +1,18 @@
 public class Author {
     private String name;
-    private String surname;
+    private int id;
+    
+    public Author(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
-    public void setName(String name) {
-        this.name = name;
+
+    public int getId() {
+        return id;
     }
-    public String getSurname() {
-        return surname;
-    }
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-    public Author(String name, String surname) {
-        this.name = name;
-        this.surname = surname;
-    }
+    
 }
