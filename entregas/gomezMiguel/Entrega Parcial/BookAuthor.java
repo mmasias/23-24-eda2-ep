939f041cb3b7cd1a458f57ba22ff272f72339d4f
miguelgomez75 <miguel.gomez@alumnos.uneatlantico.es>
2024-04-07 22:31:13 +0200

    Se montó la clase BookAuthor con los atributos int de id tanto del autor como del libro

--- entregas/gomezMiguel/Entrega Parcial/BookAuthor.java
+++ entregas/gomezMiguel/Entrega Parcial/BookAuthor.java
@@ -1,3 +1,26 @@
 public class BookAuthor {
-    
+    private int bookid;
+    private int authorid;
+
+    public BookAuthor(int bookid, int authorid) {
+        this.bookid = bookid;
+        this.authorid = authorid;
+    }
+
+    public int getbookid() {
+        return bookid;
+    }
+
+    public void setbookid(int bookid) {
+        this.bookid = bookid;
+    }
+
+    public int getauthorid() {
+        return authorid;
+    }
+
+    public void setauthorid(int authorid) {
+        this.authorid = authorid;
+    }
+
 }
