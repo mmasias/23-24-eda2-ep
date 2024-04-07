949bd4e546a46c58c230b1cd4531a979d5b324fb
miguelgomez75 <miguel.gomez@alumnos.uneatlantico.es>
2024-04-07 22:39:38 +0200

    Se hizo la clase Book con los atributos pedidos y se añadieron los metodos toString para imprimir los contenidos de ambas clases de manera ordenada

--- entregas/gomezMiguel/Entrega Parcial/Author.java
+++ entregas/gomezMiguel/Entrega Parcial/Author.java
@@ -22,4 +22,9 @@ public class Author {
     public void setid(int id) {
         this.id = id;
     }
+
+    @Override
+    public String toString() {
+        return "Autor: " + name + "  ID: " + id;
+    }
 }
