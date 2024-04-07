fbd770c8c40b9c848637b0ac9f6d4ecc50299770
pabloocanteroo <pablo.cantero@alumnos.uneatlantico.es>
2024-04-07 19:50:37 +0200

    Creo clases para hacer posible la relacion KeyWord-Document

--- entregas/canteroPablo/parcial/Document.java
+++ entregas/canteroPablo/parcial/Document.java
@@ -5,14 +5,11 @@ public class Document {
     private String tittle;
     private int publicationYear;
     private String typeDocument;
-    private ArrayList<String> keyWords;
-    public Document(int id, String tittle,  int publicationYear, String typeDocument,
-            ArrayList<String> keyWords) {
+    public Document(int id, String tittle,  int publicationYear, String typeDocument) {
         this.id = id;
         this.tittle = tittle;
         this.publicationYear = publicationYear;
         this.typeDocument = typeDocument;
-        this.keyWords = keyWords;
     }
     public int getId() {
         return id;
---
+++ entregas/canteroPablo/parcial/DocumentKeyWord.java
@@ -0,0 +1,15 @@
+public class DocumentKeyWord {
+    private int documentId;
+    private int keyWordId;
+    public DocumentKeyWord(int documentId, int keyWordId) {
+        this.documentId = documentId;
+        this.keyWordId = keyWordId;
+    }
+    public int getDocumentId() {
+        return documentId;
+    }
+    public int getKeyWordId() {
+        return keyWordId;
+    }
+    
+}
