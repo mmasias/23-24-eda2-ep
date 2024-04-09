c12b300b867ed02a75ae117fcec915e5f61c392f
Xm3Ga <adripbahamont@gmail.com>
2024-04-09 17:18:46 +0200

    Add old reto03

---
+++ entregas/adrianPerez/Reto03Antiguo/Main.java
@@ -0,0 +1,10 @@
+package entregas.adrianPerez.Reto03Antiguo;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        Biblioteca biblioteca = new Biblioteca(new ArrayList<Documento>());
+        biblioteca.menu();
+    }
+}
