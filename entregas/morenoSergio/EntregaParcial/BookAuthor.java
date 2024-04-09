f568b808d6ebc92c9f5af28b60155f8ef898165f
srgiom <113471709+srgiom@users.noreply.github.com>
2024-04-09 19:14:29 +0200

    Segundo commit

--- entregas/morenoSergio/EntregaParcial/BookAuthor.java
+++ entregas/morenoSergio/EntregaParcial/BookAuthor.java
@@ -15,7 +15,6 @@ public class BookAuthor {
         return this.idAutor;
     }
 
-    // Este método toString es opcional, dependiendo de si necesitas una representación en String de la relación
     public String toString() {
         return "Relación Libro-Autor [ID Libro=" + idLibro + ", ID Autor=" + idAutor + "]";
     }
