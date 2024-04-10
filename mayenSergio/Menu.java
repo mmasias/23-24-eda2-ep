861cafef0a8be9d777622e56f320142e6ee23297
Sergio Mayen <sergio.mayen@ct.uneatlantico.es>
2024-04-10 23:29:30 +0200

    Aportación ocho: declarar un solo metodo de menú para gestionar Autores y Documentos

--- mayenSergio/Menu.java
+++ mayenSergio/Menu.java
@@ -25,10 +25,10 @@ public class Menu {
 
             switch (opcion) {
                 case 1:
-                    gestionarDocumentos();
+                    gestionarOpcion(opcion);
                     break;
                 case 2:
-                    gestionarAutores();
+                    gestionarOpcion(opcion);
                     break;
                 case 3:
                     System.out.println("Saliendo del programa. ¡Hasta luego!");
@@ -39,135 +39,137 @@ public class Menu {
         } while (opcion != 3);
     }
 
-    private void gestionarDocumentos() {
-        int opcion;
+    private void gestionarOpcion(int opcion) {
+        int operacion;
 
-        do {
-            System.out.println("=== Gestionar Documentos ===");
-            System.out.println("1. Crear documento");
-            System.out.println("2. Mostrar lista de documentos");
-            System.out.println("3. Editar documento");
-            System.out.println("4. Eliminar documento");
-            System.out.println("5. Buscar documento");
-            System.out.println("6. Volver al menú principal");
+        if(opcion == 1){
 
-            opcion = scanner.nextInt();
-            scanner.nextLine();
+            do {
+                System.out.println("=== Gestionar Documentos ===");
+                System.out.println("1. Crear documento");
+                System.out.println("2. Mostrar lista de documentos");
+                System.out.println("3. Editar documento");
+                System.out.println("4. Eliminar documento");
+                System.out.println("5. Buscar documento");
+                System.out.println("6. Volver al menú principal");
 
-            switch (opcion) {
-                case 1:
-                    if (autores != null) {
-                        if (documentos == null) {
-                            documentos = generadorDocumentos.crearNuevoDocumento(autores);
+                operacion = scanner.nextInt();
+                scanner.nextLine();
+
+                switch (operacion) {
+                    case 1:
+                        if (autores != null) {
+                            if (documentos == null) {
+                                documentos = generadorDocumentos.crearNuevoDocumento(autores);
+                            } else {
+                                documentos.add((Documento) generadorDocumentos.crearNuevoDocumento(autores));
+                                System.out.println("Nuevo documento creado.\n");
+                            }
                         } else {
-                            documentos.add((Documento) generadorDocumentos.crearNuevoDocumento(autores));
-                            System.out.println("Nuevo documento creado.\n");
+                            System.out.println("No hay autores disponibles. Cree al menos uno primero.\n");
                         }
-                    } else {
-                        System.out.println("No hay autores disponibles. Cree al menos uno primero.\n");
-                    }
-                    break;
-                case 2:
-                    if (documentos != null) {
-                        System.out.println("\nLista de documentos:");
-                        for (Documento documento : documentos) {
-                            System.out.println(documento);
+                        break;
+                    case 2:
+                        if (documentos != null) {
+                            System.out.println("\nLista de documentos:");
+                            for (Documento documento : documentos) {
+                                System.out.println(documento);
+                            }
+                            System.out.println();
+                        } else {
+                            System.out.println("No hay documentos para mostrar. Crea uno primero.\n");
                         }
-                        System.out.println();
-                    } else {
-                        System.out.println("No hay documentos para mostrar. Crea uno primero.\n");
-                    }
-                    break;
-                case 3:
-                    if (documentos != null) {
-                        generadorDocumentos.editarDocumento(documentos);
-                    } else {
-                        System.out.println("No hay documentos para editar. Crea uno primero.\n");
-                    }
-                    break;
-                case 4:
-                    if (documentos != null) {
-                        generadorDocumentos.eliminarDocumento(documentos);
-                    } else {
-                        System.out.println("No hay documentos para eliminar. Crea uno primero.\n");
-                    }
-                    break;
-                case 5:
-                    if (documentos != null) {
-                        generadorDocumentos.buscarDocumento(documentos);
-                    } else {
-                        System.out.println("No hay documentos para buscar. Crea uno primero.\n");
-                    }
-                    break;
-                case 6:
-                    return;
-                default:
-                    System.out.println("Opción no válida. Inténtelo de nuevo.\n");
-            }
-        } while (opcion != 6);
-    }
-
-    private void gestionarAutores() {
-        int opcion;
-
-        do {
-            System.out.println("=== Gestionar Autores ===");
-            System.out.println("1. Crear autor");
-            System.out.println("2. Mostrar lista de autores");
-            System.out.println("3. Editar autor");
-            System.out.println("4. Eliminar autor");
-            System.out.println("5. Buscar autor");
-            System.out.println("6. Volver al menú principal");
-
-            opcion = scanner.nextInt();
-            scanner.nextLine();
-
-            switch (opcion) {
-                case 1:
-                    if (autores == null) {
-                        autores = generadorAutores.crearNuevoAutor();
-                    } else {
-                        autores.addAll(generadorAutores.crearNuevoAutor());
-                        System.out.println("Nuevos autores creados.\n");
-                    }
-                    break;
-                case 2:
-                    if (autores != null) {
-                        System.out.println("\nLista de autores:");
-                        for (Autor autor : autores) {
-                            System.out.println(autor);
+                        break;
+                    case 3:
+                        if (documentos != null) {
+                            generadorDocumentos.editarDocumento(documentos);
+                        } else {
+                            System.out.println("No hay documentos para editar. Crea uno primero.\n");
                         }
-                        System.out.println();
-                    } else {
-                        System.out.println("No hay autores para mostrar. Crea uno primero.\n");
-                    }
-                    break;
-                case 3:
-                    if (autores != null) {
-                        generadorAutores.editarAutor(autores);
-                    } else {
-                        System.out.println("No hay autores para editar. Crea uno primero.\n");
-                    }
-                    break;
-                case 4:
-                    if (autores != null) {
-                        generadorAutores.eliminarAutor(autores);
-                    } else {
-                        System.out.println("No hay autores para eliminar. Crea uno primero.\n");
-                    }
-                    break;
-                case 5:
-                    if (autores != null) {
-                        generadorAutores.buscarAutor(autores);
-                    } else {
-                        System.out.println("No hay autores para buscar. Crea uno primero.\n");
-                    }
-                    break;
-                case 6:
-                    return;
-                default:
-                    System.out.println("Opción no válida. Inténtelo de nuevo.\n");
-            }
-        } while (opcion != 6);
+                        break;
+                    case 4:
+                        if (documentos != null) {
+                            generadorDocumentos.eliminarDocumento(documentos);
+                        } else {
+                            System.out.println("No hay documentos para eliminar. Crea uno primero.\n");
+                        }
+                        break;
+                    case 5:
+                        if (documentos != null) {
+                            generadorDocumentos.buscarDocumento(documentos);
+                        } else {
+                            System.out.println("No hay documentos para buscar. Crea uno primero.\n");
+                        }
+                        break;
+                    case 6:
+                        return;
+                    default:
+                        System.out.println("Opción no válida. Inténtelo de nuevo.\n");
+                }
+            } while (operacion != 6);
+
+        } else {
+
+            do {
+                System.out.println("=== Gestionar Autores ===");
+                System.out.println("1. Crear autor");
+                System.out.println("2. Mostrar lista de autores");
+                System.out.println("3. Editar autor");
+                System.out.println("4. Eliminar autor");
+                System.out.println("5. Buscar autor");
+                System.out.println("6. Volver al menú principal");
+
+                operacion = scanner.nextInt();
+                scanner.nextLine();
+
+                switch (operacion) {
+                    case 1:
+                        if (autores == null) {
+                            autores = generadorAutores.crearNuevoAutor();
+                        } else {
+                            autores.addAll(generadorAutores.crearNuevoAutor());
+                            System.out.println("Nuevos autores creados.\n");
+                        }
+                        break;
+                    case 2:
+                        if (autores != null) {
+                            System.out.println("\nLista de autores:");
+                            for (Autor autor : autores) {
+                                System.out.println(autor);
+                            }
+                            System.out.println();
+                        } else {
+                            System.out.println("No hay autores para mostrar. Crea uno primero.\n");
+                        }
+                        break;
+                    case 3:
+                        if (autores != null) {
+                            generadorAutores.editarAutor(autores);
+                        } else {
+                            System.out.println("No hay autores para editar. Crea uno primero.\n");
+                        }
+                        break;
+                    case 4:
+                        if (autores != null) {
+                            generadorAutores.eliminarAutor(autores);
+                        } else {
+                            System.out.println("No hay autores para eliminar. Crea uno primero.\n");
+                        }
+                        break;
+                    case 5:
+                        if (autores != null) {
+                            generadorAutores.buscarAutor(autores);
+                        } else {
+                            System.out.println("No hay autores para buscar. Crea uno primero.\n");
+                        }
+                        break;
+                    case 6:
+                        return;
+                    default:
+                        System.out.println("Opción no válida. Inténtelo de nuevo.\n");
+                }
+            } while (operacion != 6);
+        }
+        
     }
 }
