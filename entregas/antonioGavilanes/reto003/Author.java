bf13124f1ad9af798e9b881d2bf6cd2bd8e35d33
Antonio Gavilanes <antonio.gavilanes@alumnos.uneatlantico.es>
2024-04-10 03:15:06 +0200

    Métodos y clases según el diagrama uml

--- entregas/antonioGavilanes/reto003/Author.java
+++ entregas/antonioGavilanes/reto003/Author.java
@@ -1,5 +1,23 @@
 package antonioGavilanes.reto003;
 
 public class Author {
-    
+    private int id;
+    private String name;
+
+    public Author(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+    }
 }
--- entregas/antonioGavilanes/reto003/LibraryManager.java
+++ entregas/antonioGavilanes/reto003/LibraryManager.java
@@ -6,4 +6,54 @@ import java.util.Scanner;
 
 public class LibraryManager {
     
+    private List<Book> books;
+    private List<Author> authors;
+    private List<BookAuthor> relations;
+    private Scanner scanner;
+
+    public LibraryManager() {
+        books = new ArrayList<>();
+        authors = new ArrayList<>();
+        relations = new ArrayList<>();
+        scanner = new Scanner(System.in);
+    }
+
+    public void startLibraryManager() {
+    }
+
+    private void listBooks() {
+    }
+
+    private void addBook() {
+    }
+
+    private void addBook(Book book) {
+    }
+
+    private void addAuthor(Book book) {
+    }
+
+    private void addAuthor(Author author) {
+    }
+
+    private void addRelation(int bookId, int authorId) {
+    }
+
+    private List<Author> getAuthorsByBookId(int bookId) {
+    }
+
+    private List<Book> getBooksByAuthorId(int authorId) {
+        return null;
+    }
+
+    private Book findBookById(int bookId) {
+        return null;
+    }
+
+    private Author findAuthorById(int authorId) {
+        return null;
+    }
+
+    private void listAuthors() {
+    }
 }
